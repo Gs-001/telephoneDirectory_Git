06640bcf4c25f2a0a188c515db39e4b5f4478afc
Gurcharan Singh <gurcharan0993@gmail.com>
2019-08-16 03:23:49 +0530

    Coarse Code

--- telephoneDirectory.py
+++ telephoneDirectory.py
@@ -1,41 +1,119 @@
-# Telephone Directory
-# language: Python
-# DS: Python Dictionary
+import re
 
 
-def searchDirectory():
-    # A single search function for everything
-    # output will be an array of possible entries
+directory = dict()
 
-def addEntry():
-    # Existance check
-    # Add to the database
-    # OR return false
+# directory = {'Gurcharan Singh':8146248862,'Naveen Bassi':2345324534,'Jagmanjot Singh':2345676543,'Sresht Shashi':2345676543,'Ankit Billa':3456543223}
 
+# ADD ENTRY function
+def addEntry(name, number):
+    directory[name] = number
 
+# PRINT DIRECTORY function
+def printDirectory():
+    for entry in directory:
+        print(entry, "-", directory[entry])
 
-if __name__ == '__main__':
-    print("Telephone Directory") # ta-da!!!
+# SEARCH ENTRY function
+def searchEntry(key,flag):
+    str = '[a-zA-Z]*'+key+'[a-zA-Z]*'
+    search = re.compile(str, re.IGNORECASE)
 
-    print("Options: \n 1. Add Entry \n 2. Delete Entry \n 3. Update Entry \n 4. Search Entry 5. Exit")
-    choice = input("What do you want to do ?")
+    if flag == False:
+        names = dict()
+        for entry in directory:
+            if search.findall(entry):
+                names[entry] = directory[entry]
+        return names
 
-while(1):
-    if(choice == '1'):
-        # Take input
-        # Add entry
-    elif(choice == '2'):
-        # Take input
-        # Search database
-        # Delete entry
-    elif(choice == '3'):
-        # Take input
-        # Search database
-        # Update entry
-    elif(choice == '4'):
-        # Take input
-        # Search database and display results
-    elif(choice == '5'):
-        # break loop
     else:
-        #wrong input
+        names = []
+        for entry in directory:
+            if search.findall(entry):
+                names.append(entry)
+        return names
+
+# REMOVE ENTRY function
+def removeEntry(name):
+    entries = searchEntry(name,True)
+    if entries is None:
+        return None
+    elif type(entries) != list:
+        del directory[entries]
+        return 1
+    index = 1
+    for entry in entries:
+        print(index,'.', entry)
+        index += 1
+    index -= 1
+    val = int(input('Which Entry to Delete?:'))
+    if val > index or val < 0:
+        print('Invalid Input')
+        return None
+    else:
+        del directory[entries[val-1]]
+        return 1
+    
+# UPDATE ENTRY function
+def updateEntry(name,number):
+    entries = searchEntry(name,True)
+    if entries is None:
+        return None
+    elif type(entries) != list:
+        directory[entries] = number
+        return 1
+    index = 1
+    for entry in entries:
+        print(index,'.', entry)
+        index += 1
+    index -= 1
+    val = int(input('Which Entry to Update?:'))
+    if val > index or val < 0:
+        print('Invalid Input')
+        return None
+    else:
+        directory[entries[val-1]] = number
+        return 1
+
+
+# MAIN 
+if __name__ == "__main__":
+    print("TELEPHONE DIRECTORY")
+    print('Actions : ')
+
+    while(True):
+        print('1. Add Entry \n 2. Remove Entry \n 3. Update Entry \n 4. Search \n 5. Print All Entries \n 6. Exit')
+
+        
+        choice = input('What do you want to do ? ')
+
+        if choice == '1':
+            name = input('Enter Contact Name:')
+            number = input('Enter Contact Number:')
+            addEntry(name,number)
+        elif choice == '2':
+            name = input('Enter full Contact Name:')
+            value = removeEntry(name)
+            if value is None:
+                print('The Entry does not exist')
+            else:
+                print('Entry Removed')
+        elif choice == '3':
+            name = input('Enter Key:')
+            number = input('Enter Number:')
+            val = updateEntry(name,number)
+            if val is None:
+                print('The Entry does not exist')
+            else:
+                print('Entry Updated')
+        elif choice == '4':
+            key = input('Enter Key to search:')
+            names = searchEntry(key,False)
+            for name in names:
+                print(name,'-',names[name])
+        elif choice == '5':
+            printDirectory()
+        elif choice == '6':
+            break
+        else:
+            print('---Invalid Input---')
